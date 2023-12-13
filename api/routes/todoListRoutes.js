07d1a600d9a9346fb629886cbda7b913af0a8edd
maksbovtuta <klarus335@ukr.net>
2023-12-13 19:34:18 +0100

    two commit

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,3 +1,137 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Task:
+ *       type: object
+ *       required:
+ *         - title
+ *         - description
+ *         - completed
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the task
+ *         title:
+ *           type: string
+ *           description: The title of your task
+ *         description:
+ *           type: string
+ *           description: Description of the task
+ *         completed:
+ *           type: boolean
+ *           description: Whether the task is completed or not
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the task was added
+ *       example:
+ *         id: d5fE_asz
+ *         title: Complete Project Report
+ *         description: Write and finalize the project report
+ *         completed: false
+ *         createdAt: 2023-12-13T08:00:00.000Z
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Tasks
+ *   description: The tasks managing API
+ * /tasks:
+ *   get:
+ *     summary: Lists all the tasks
+ *     tags: [Tasks]
+ *     responses:
+ *       200:
+ *         description: The list of tasks
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Task'
+ *   post:
+ *     summary: Create a new task
+ *     tags: [Tasks]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       200:
+ *         description: The created task
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       500:
+ *         description: Some server error
+ * /tasks/{id}:
+ *   get:
+ *     summary: Get a task by its ID
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task ID
+ *     responses:
+ *       200:
+ *         description: The task response by ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       404:
+ *         description: The task was not found
+ *   put:
+ *     summary: Update a task by its ID
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Task'
+ *     responses:
+ *       200:
+ *         description: The task was updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Task'
+ *       404:
+ *         description: The task was not found
+ *       500:
+ *         description: Some error happened
+ *   delete:
+ *     summary: Remove a task by its ID
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The task ID
+ *     responses:
+ *       200:
+ *         description: The task was deleted
+ *       404:
+ *         description: The task was not found
+ */
 'use strict';
 module.exports = function(app) {
   var todoList = require('../controllers/todoListController');
@@ -12,4 +146,6 @@ module.exports = function(app) {
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
+
+    
 };
--- server.js
+++ server.js
@@ -3,15 +3,57 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Task = require('./api/models/todoListModel'), //created model loading here
-  bodyParser = require('body-parser');
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
   
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/Tododb'); 
 
-app.use(function(req, res) {
-    res.status(404).send({url: req.originalUrl + ' not found'})
-  });
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./api/routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, {
+    explorer: true,
+    customCssUrl:
+      "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+  })
+);
+  
+
+// app.use(function(req, res) {
+//     res.status(404).send({url: req.originalUrl + ' not found'})
+//   });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
